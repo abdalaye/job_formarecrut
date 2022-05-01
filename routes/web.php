16e0d8067de5bc7ef98c74aadbd43b2693f70839
Abdalaye Soumare <asoumare@afridev-group.com>
2022-05-01 14:45:58 +0000

    Correction home

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/tableau-de-bord', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/tableau-de-bord', [App\Http\Controllers\HomeController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/compte', function() {
