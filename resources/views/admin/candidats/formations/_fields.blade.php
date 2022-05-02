a41e6938d72594c9c7dacf8625deb68c62499006
Papa Mouhamadou DIOP <kaiserification@gmail.com>
2022-05-02 21:24:00 +0000

    mise à jour du fichier .env.example

--- resources/views/admin/candidats/formations/_fields.blade.php
+++ resources/views/admin/candidats/formations/_fields.blade.php
@@ -2,18 +2,17 @@
     <div class="col-12">
         <div class="form-group">
             {!! Form::label('titre', 'Formation', ['class' => 'control-label']) !!}
-            {!! Form::text('titre', null, ['class' => 'form-control '.errorField($errors, 'titre')]) !!}
+            {!! Form::text('titre', null, ['class' => 'form-control '.errorField($errors, 'titre'), 'required' => true]) !!}
             @error('titre') <span class="invalid-feedback">{{ $message }}</span> @enderror
         </div>
     </div>
 </div>
 
-
 <div class="row">
     <div class="col-12">
         <div class="form-group">
             {!! Form::label('etablissement', 'Etablissement', ['class' => 'control-label']) !!}
-            {!! Form::text('etablissement', null, ['class' => 'form-control '.errorField($errors, 'etablissement')]) !!}
+            {!! Form::text('etablissement', null, ['class' => 'form-control '.errorField($errors, 'etablissement'), 'required' => true]) !!}
             @error('etablissement') <span class="invalid-feedback">{{ $message }}</span> @enderror
         </div>
     </div>
@@ -23,7 +22,7 @@
     <div class="col-6">
         <div class="form-group">
             {!! Form::label('country_id', 'Pays', ['class' => 'control-label']) !!}
-            {!! Form::select('country_id', ['' => 'Pays'] + \App\Models\Country::active()->pluck('name', 'id')->all(), null, ['class' => 'form-control '.errorField($errors, 'country_id')]) !!}
+            {!! Form::select('country_id', ['' => 'Pays'] + \App\Models\Country::active()->pluck('name', 'id')->all(), null, ['class' => 'form-control '.errorField($errors, 'country_id'), 'required' => true]) !!}
             @error('country_id') <span class="invalid-feedback">{{ $message }}</span> @enderror
         </div>
     </div>
@@ -31,7 +30,7 @@
     <div class="col-6">
         <div class="form-group">
             {!! Form::label('city_id', 'Ville', ['class' => 'control-label']) !!}
-            {!! Form::select('city_id', ['' => 'Ville'] + \App\Models\City::active()->pluck('name', 'id')->all(), null, ['class' => 'form-control '.errorField($errors, 'city_id')]) !!}
+            {!! Form::select('city_id', ['' => 'Ville'] + \App\Models\City::active()->pluck('name', 'id')->all(), null, ['class' => 'form-control '.errorField($errors, 'city_id'), 'required' => true]) !!}
             @error('city_id') <span class="invalid-feedback">{{ $message }}</span> @enderror
         </div>
     </div>
@@ -42,7 +41,7 @@
     <div class="col-6">
         <div class="form-group">
             {!! Form::label('date_debut', 'Date de début', ['class' => 'control-label']) !!}
-            {!! Form::date('date_debut', null, ['class' => 'form-control '.errorField($errors, 'date_debut')]) !!}
+            {!! Form::date('date_debut', null, ['class' => 'form-control '.errorField($errors, 'date_debut'), 'required' => true]) !!}
             @error('date_debut') <span class="invalid-feedback">{{ $message }}</span> @enderror
         </div>
     </div>
@@ -59,7 +58,7 @@
                 </div>
             </div>
 
-            {!! Form::date('date_fin', null, ['class' => 'form-control '.errorField($errors, 'date_fin')]) !!}
+            {!! Form::date('date_fin', null, ['class' => 'form-control '.errorField($errors, 'date_fin'), 'required' => true]) !!}
             @error('date_fin') <span class="invalid-feedback">{{ $message }}</span> @enderror
         </div>
     </div>
